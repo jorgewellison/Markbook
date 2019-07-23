96bcd277af9d1f1cc1c72968fb7f2ddb04c4d4b7
jorgewellison <jorge.wellison@dce.ufpb.br>
2019-07-22 22:22:43 -0300

    Tela de login adicionada ao home, faltando completar a ligacao com o authservice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,36 +5,24 @@ import { ReactiveFormsModule} from '@angular/forms';
 import { RouterModule, Routes} from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-
-const appRoutes: Routes = [
-  {
-    path: '',
-    component: LoginComponent
- },
- {
-    path: 'app-home',
-    component: HomeComponent
- }
-];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LoginComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthServiceService } from './../auth-service.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  message: string;
+  constructor(public authService: AuthServiceService) {
+    this.message = '';
+  }
+  login(username: string, password: string): boolean {
+    this.message = '';
+    if (!this.authService.login(username, password)) {
+      this.message = 'Incorrect credentials.';
+      setTimeout(function() {
+        this.message = '';
+      }.bind(this), 2500);
+    }
+    return false;
+  }
+  logout(): boolean { this.authService.logout(); return false; }
+
 
   ngOnInit() {
-    document.body.classList.add('bg-img');
-  }
 
+  }
 }
