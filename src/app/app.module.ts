a60b23a56a19acce9f951f80cfb2ad396ff6c6d4
jorgewellison <jorge.wellison@dce.ufpb.br>
2019-07-29 21:28:03 -0300

    Deletando o username do localstorage e ocultando navbar ao ser feito o logout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,16 +14,9 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { CarrosselLivrosComponent } from './carrossel-livros/carrossel-livros.component';
-import { CarrosselLivrosService } from './carrossel-livros/carrossel-livros.service';
 import { ExibeLivrosComponent } from './exibe-livros/exibe-livros.component';
-import { LivroComponent } from './livro/livro.component';
 import { UploadFileComponent } from './upload-file/upload-file/upload-file.component';
 import { LoggedGuard } from './logged.guard';
-import { NovolivroComponent } from './novolivro/novolivro.component';
-import { DetalheslivroComponent } from './detalheslivro/detalheslivro.component';
-import { EditarLivroComponent } from './editar-livro/editar-livro.component';
-import { EditarPerfilComponent } from './editar-perfil/editar-perfil.component';
 
 @NgModule({
   declarations: [
@@ -32,15 +25,9 @@ import { EditarPerfilComponent } from './editar-perfil/editar-perfil.component';
     FooterComponent,
     HomeComponent,
     DashboardComponent,
-    CarrosselLivrosComponent,
     ExibeLivrosComponent,
-    LivroComponent,
     UploadFileComponent,
-    NavComponent,
-    NovolivroComponent,
-    DetalheslivroComponent,
-    EditarLivroComponent,
-    EditarPerfilComponent
+    NavComponent
   ],
   imports: [
     BrowserModule,
@@ -50,7 +37,6 @@ import { EditarPerfilComponent } from './editar-perfil/editar-perfil.component';
   ],
   providers: [
     AuthServiceService,
-    CarrosselLivrosService,
     LoggedGuard
   ],
   bootstrap: [AppComponent]
