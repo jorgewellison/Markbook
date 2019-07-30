2e34728a53f62c57a3977c97f5a1389f62cbf1d6
Jorge <jorge.wellison@dce.ufpb.br>
2019-07-30 09:52:59 -0300

    componente de mostrar os livros

--- src/app/livro/mostrar-livro/mostrar-livro.component.ts
+++ src/app/livro/mostrar-livro/mostrar-livro.component.ts
@@ -1,5 +1,12 @@
-import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import 'rxjs/add/operator/catch';
+import { LivroService } from './../livro.service';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Livro } from '../livro';
+import { resolve } from 'q';
+import 'rxjs/Rx';
+
 
 @Component({
   selector: 'app-mostrar-livro',
@@ -7,10 +14,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mostrar-livro.component.css']
 })
 export class MostrarLivroComponent implements OnInit {
+  public livro: Livro;
 
-  constructor(private Route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private livroService: LivroService) { }
 
   ngOnInit() {
+    this.route.params.switchMap( (params: Params) => this.carregarLivro(+params['id']) )
+    .subscribe( (livro: Livro) => this.livro = livro);
   }
 
+  carregarLivro(id: number) : Promise<Livro> {
+    return new Promise ( (resolve) => resolve(this.livroService.getById(id)));
+  }
 }
