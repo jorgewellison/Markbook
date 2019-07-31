95251c7c5fe6fc4c67861e802041824eb517b1e1
Jorge <jorge.wellison@dce.ufpb.br>
2019-07-30 22:01:44 -0300

    Criaçao dos novos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
+import { MinhaBibliotecaComponent } from './minha-biblioteca/minha-biblioteca.component';
+import { EditarPerfilComponent } from './editar-perfil/editar-perfil.component';
 import { EditarLivroComponent } from './livro/editar-livro/editar-livro.component';
-import { MostrarLivroComponent } from './livro/mostrar-livro/mostrar-livro.component';
 import { NovoLivroComponent } from './livro/novo-livro/novo-livro.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
@@ -7,7 +8,6 @@ import { Routes, RouterModule, CanActivate } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ListaDeLivrosComponent } from './livro/lista-de-livros/lista-de-livros.component';
 import { AuthGuardService } from './guards/auth.guard';
-import { PgLeituraComponent } from './pg-leitura/pg-leitura/pg-leitura.component';
 
 
 const routes: Routes = [
@@ -18,7 +18,9 @@ const routes: Routes = [
   { path: 'dashboard/livros', component: ListaDeLivrosComponent, canActivate: [AuthGuardService]},
   { path: 'dashboard/livros/novo', component: NovoLivroComponent, canActivate: [AuthGuardService]},
   { path: 'dashboard/livro:id/edit', component: EditarLivroComponent, canActivate: [AuthGuardService]},
-  { path: 'pg-leitura', component: PgLeituraComponent, canActivate: [AuthGuardService]},
+  { path: 'dashboard/editar-perfil', component: EditarPerfilComponent, canActivate: [AuthGuardService]},
+  { path: 'dashboard/minha-biblioteca', component: MinhaBibliotecaComponent, canActivate: [AuthGuardService]},
+
 ];
 
 @NgModule({
