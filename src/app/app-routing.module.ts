0567702dc625cfd72c155ec0118b77ff9b88325b
Jorge <jorge.wellison@dce.ufpb.br>
2019-08-10 02:23:24 -0300

    Correçao do dashboard e das rotas + componente detalhes-livro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { DetalhesLivroComponent } from './livro/detalhes-livro/detalhes-livro.component';
 import { MinhaBibliotecaComponent } from './minha-biblioteca/minha-biblioteca.component';
 import { EditarPerfilComponent } from './usuario/editar-perfil/editar-perfil.component';
 import { EditarLivroComponent } from './livro/editar-livro/editar-livro.component';
@@ -13,15 +14,18 @@ import { PgLeituraComponent } from './pg-leitura/pg-leitura/pg-leitura.component
 
 const routes: Routes = [
 
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', component: HomeComponent},
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuardService]},
-  { path: 'dashboard/livros', component: ListaDeLivrosComponent, canActivate: [AuthGuardService]},
-  { path: 'dashboard/livros/novo', component: NovoLivroComponent, canActivate: [AuthGuardService]},
-  { path: 'dashboard/livro:id/edit', component: EditarLivroComponent, canActivate: [AuthGuardService]},
-  { path: 'dashboard/editar-perfil', component: EditarPerfilComponent, canActivate: [AuthGuardService]},
-  { path: 'dashboard/minha-biblioteca', component: MinhaBibliotecaComponent, canActivate: [AuthGuardService]},
-  { path: 'pg-leitura', component: PgLeituraComponent, canActivate: [AuthGuardService]},
-
+  { path: 'livros', component: ListaDeLivrosComponent, canActivate: [AuthGuardService]},
+  { path: 'dashboard/livros:id', component: DetalhesLivroComponent, canActivate: [AuthGuardService],
+    children: [
+      { path: '', redirectTo: 'edit', pathMatch: 'full'},
+      { path: 'edit', component: EditarLivroComponent, canActivate: [AuthGuardService]},
+      { path: 'pg-leitura', component: PgLeituraComponent, canActivate: [AuthGuardService]},
+    ]},
+  { path: 'editar-perfil', component: EditarPerfilComponent, canActivate: [AuthGuardService]},
+  { path: 'novo', component: NovoLivroComponent, canActivate: [AuthGuardService]},
+  { path: 'minha-biblioteca', component: MinhaBibliotecaComponent, canActivate: [AuthGuardService]},
 ];
 
 @NgModule({
---
+++ src/app/livro/detalhes-livro/detalhes-livro.component.ts
@@ -0,0 +1,18 @@
+import { LivroService } from './../livro.service';
+import { Component, OnInit } from '@angular/core';
+import { Livro } from '../livro';
+
+@Component({
+  selector: 'app-detalhes-livro',
+  templateUrl: './detalhes-livro.component.html',
+  styleUrls: ['./detalhes-livro.component.css']
+})
+export class DetalhesLivroComponent implements OnInit {
+
+  constructor(private livroService : LivroService) { }
+
+  ngOnInit() {
+    this.livroService.getLivros();
+  }
+
+}
